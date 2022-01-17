b22d8206999d275bcd7f4449bfc6eee519e8e99f
fun <lw_fun@163.com>
2022-01-17 15:11:02 +0800

    删除无用

--- Sources/Popover/Toolbox/DispatchQueue.swift
+++ Sources/Popover/Toolbox/DispatchQueue.swift
@@ -4,43 +4,10 @@ extension DispatchQueue {
     func delay(_ delay: TimeInterval = 0, execute: @escaping () -> Void) {
         asyncAfter(deadline: .now() + delay, execute: execute)
     }
-    
-    func delay(_ delay: TimeInterval = 0, execute: DispatchWorkItem) {
-        asyncAfter(deadline: .now() + delay, execute: execute)
-    }
 }
 
 /// Delay on main queue.
 func delay(_ delay: TimeInterval = 0, execute: @escaping () -> Void) {
     DispatchQueue.main.delay(delay, execute: execute)
-}
-
-/// Delay on main queue.
-func delay(_ delay: TimeInterval = 0, execute: DispatchWorkItem) {
-    DispatchQueue.main.delay(delay, execute: execute)
-}
-
-extension DispatchQueue {
-    /// Dispatch the block to main queue asynchronously if needed.
-    static func onMainAsync(_ block: @escaping () -> Void) {
-        if Thread.isMainThread {
-            block()
-        } else {
-            DispatchQueue.main.async {
-                block()
-            }
-        }
-    }
-    
-    /// Dispatch the block to main queue synchronously if needed.
-    static func onMainSync(_ block: @escaping () -> Void) {
-        if Thread.isMainThread {
-            block()
-        } else {
-            DispatchQueue.main.sync {
-                block()
-            }
-        }
-    }
 }
 
